3c16cce005b4568fd7aeff76744e25160b315bbc
tonykastaneda <tonykastaneda@gmail.com>
2021-08-22 19:57:54 -0700

    Update new-markdown.sh

--- workflows/new-markdown.sh
+++ workflows/new-markdown.sh
@@ -8,15 +8,13 @@
 # Optional parameters:
 # @raycast.icon 🔻
 # @raycast.argument1 { "type": "text", "placeholder": "Name.md" }
-# @raycast.packageName newmd
+# @raycast.packageName mdnew
 
 # Documentation:
 # @raycast.description creates a new .md file in the Masterloot folder on iCloud
 # @raycast.author Tony
 
-cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Masterloot
+cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/Masterloot;
+touch $1.md;
 open $1.md
-
-
-echo ""
 
