206780ad4346bd7089348a660a25124797cd6803
tonykastaneda <tonykastaneda@gmail.com>
2021-08-23 16:57:08 -0700

    nvim open script

---
+++ workflows/nvim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title nvim
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName nvim
+
+# Documentation:
+# @raycast.description Opens nvim in iTerm
+# @raycast.author Tony
+
+open -a "iTerm" && nvim 
+
