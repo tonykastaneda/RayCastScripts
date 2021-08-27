10100484a9b40d6f52a45cadc53830b814865797
tonykastaneda <tonykastaneda@gmail.com>
2021-08-26 20:09:24 -0700

    mout test

---
+++ workflows/mount-clouds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Mount Google Drive
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ☁️
+# @raycast.packageName Bootleg Mountain Duck
+
+# Documentation:
+# @raycast.description Mounts clouds with rclone-cli
+# @raycast.author Tony
+
+rclone mount tony_drive: ~/tony_drive;
+
+echo "Quak 🦆"
+
