4b4b87abeec65a076d1ef066c889bf6476c6c51e
tonykastaneda <tonykastaneda@gmail.com>
2021-09-01 21:01:15 -0700

    d

---
+++ workflows/yabai-on.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Yabai ON
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🦾
+# @raycast.packageName yabai-on
+
+# Documentation:
+# @raycast.description Tunrs on Yabai
+# @raycast.author Tony
+
+brew services start yabai
+
+
+echo "Rebooting"
+
